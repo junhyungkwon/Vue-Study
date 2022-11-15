19fc22e9fe5649992611afd2557f320e0e9a9472
junhyungkwon <wnsgud8845@gmail.com>
2022-11-16 02:17:42 +0900

    v-if사용하여 창 만들고, 닫기 버튼누르면 창 닫히게 구현하기.

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,27 +1,43 @@
 <template>
 
+
+<div class="black-bg" v-if="modalopen == true"> 
+<div class="white-bg">
+  <h4>상세페이지</h4>
+  <p>상세페이지 내용</p>
+  <button @click="modalopen = false"> 닫기 </button>
+   </div>
+</div>
+
 <div class= "menu">
-  <a href ="" a v-for="testcode in menus" :key="testcode"> {{testcode}} </a>
+  <a  a v-for="testcode in menus" :key="testcode"> {{testcode}} </a>
   
 
    </div>
   
 
   <div>
+    <img src="./assets/room0.jpg" class="room-img" >
     <h4>{{products[0]}}</h4>
      <p> {{price[0]}} 만원</p>
-     <button @click= "increase">허위매물 신고</button>
-     <span>신고수: {{신고수}} </span>
+     <button @click= "신고수[0]++">허위매물 신고</button>
+     <span>신고수: {{신고수[0]}} </span>
    </div>
    
    <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4>{{products[1]}}</h4>
      <p> {{price[1]}} 만원</p>
+     <button @click= "신고수[1]++">허위매물 신고</button>
+     <span>신고수: {{신고수[1]}} </span>
    </div>
 
    <div>
+    <img src="./assets/room2.jpg" class="room-img" >
     <h4>{{products[2]}}</h4>
      <p> {{price[2]}} 만원</p>
+     <button @click= "신고수[2]++">허위매물 신고</button>
+     <span>신고수: {{신고수[2]}} </span>
    </div>
    
   
@@ -34,7 +50,8 @@ export default {
   name: 'App',
   data() {
     return {
-      신고수: 0,
+      modalopen: true,
+      신고수: [0,0,0],
       price: [60, 70, 90],
       menus: ['Home', 'Shop', 'About'],
       products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
@@ -42,10 +59,11 @@ export default {
     }
   },
 
-  methods : {
+  methods: {
     increase() {
       this.신고수 += 1;
     }
+    
   },
 
   components: {
@@ -55,6 +73,24 @@ export default {
 </script>
 
 <style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -73,4 +109,9 @@ export default {
   color: white;
   padding: 15px;
 }
+.room-img{
+  width: 100%;
+  margin-top: 40px;
+}
+
 </style>
