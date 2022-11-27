6f5f02b2428faa600631345d4fb91adc1c6b4555
junhyungkwon <wnsgud8845@gmail.com>
2022-11-27 17:25:16 +0900

    v-mode 사용해서 사용자 input받기

--- vuedongsan/src/Modal.vue
+++ vuedongsan/src/Modal.vue
@@ -4,7 +4,8 @@
    <img :src="onerooms[누른거].image">
   <h4>{{ onerooms[누른거].title }}</h4>
   <p>{{onerooms[누른거].content}}</p>
-  <p>{{onerooms[누른거].price}} 원</p>
+  <input v-model="month">
+  <p>{{ month }}개월 선택함 : {{onerooms[누른거].price * month}} 원</p>
   <button @click="$emit('modalclose')"> 닫기 </button> 
  <!-- <button @click="modalopen = false"> 닫기 </button>  -->
    </div>
@@ -14,6 +15,11 @@
 <script>
 export default {
     name : "Modal",
+    data(){
+      return {
+        month: 1,
+      }
+    },
 props: {
     onerooms: Array,
     누른거: Number,
